368f85074a20720d3ad997046bc9d2f166d864d9
Fedczuk Alex <afedczuk@indec.gob.ar>
2023-10-24 09:58:17 -0300

    ...

--- classes.py
+++ classes.py
@@ -6,6 +6,7 @@ from os.path import isfile, join
 
 from pygame.sprite import Group
 from constants import COLOR, GRAVITY, ANIMATION_DELAY
+#from funtions import load_sprite_sheets
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, load_sprite_sheets) -> None:
@@ -110,3 +111,33 @@ class Block(Object):
 
     def blit(self, surface:pygame.Surface, offset_x:int):
         surface.blit(self.image, (self.rect.x - offset_x, self.rect.y))
+
+class Fire(Object):
+    def __init__(self, x, y, width, height, load_sprite_sheets) -> None:
+        super().__init__(x, y, width, height, "fire")
+        self.fire = load_sprite_sheets("Traps", "Fire", width, height)
+        self.image = self.fire["off"][0]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.animation_count = 0
+        self.animation_name = "off"
+
+    def on(self):
+        self.animation_name = "on"
+
+    def off(self):
+        self.animation_name = "off"
+
+    def loop(self):
+        sprites = self.fire[self.animation_name]
+        sprite_index = (self.animation_count // ANIMATION_DELAY) % len(sprites)
+        self.image = sprites[sprite_index]
+        self.animation_count += 1
+
+        # Update
+        self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
+
+        if self.animation_count // ANIMATION_DELAY > len(sprites):
+            self.animation_count = 0
+
+    
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pygame
 from pygame.sprite import Group
 from constants import WIDTH, HEIGHT, FPS
 from funtions import get_background, handle_move, draw, load_sprite_sheets, get_block
-from classes import Player, Block
+from classes import Player, Block, Fire
 
 pygame.init()
 
@@ -23,11 +23,15 @@ def main(window):
     player = Player(100, 100, 50, 50, load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True))
     # blocks = [Block(0, HEIGHT - block_size, block_size, get_block)]
     floor = [Block(i * block_size, HEIGHT - block_size, block_size, get_block) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
+    fire = Fire(100, HEIGHT - block_size - 64, 16, 32, load_sprite_sheets)
+    fire.on()
     objects = [*floor,
                Block(0, HEIGHT - block_size * 2, block_size, get_block),
                Block(block_size * 3, HEIGHT - block_size * 4, block_size, get_block),
                Block(block_size * 4, HEIGHT - block_size * 4, block_size, get_block),
-               Block(block_size * 6, HEIGHT - block_size * 5, block_size, get_block)]
+               Block(block_size * 6, HEIGHT - block_size * 5, block_size, get_block),
+               fire]
+    
 
     offset_x = 0
     scroll_area_width = 200
