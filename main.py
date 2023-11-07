0c0ff42c548b2962af2ef6efcfc7cc749ea326dd
Fedczuk Alex <afedczuk@indec.gob.ar>
2023-11-07 08:53:15 -0300

    good

--- main.py
+++ main.py
@@ -16,25 +16,26 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image = get_background("Blue.png")
-
-    block_size = 96
-
-    player = Player(100, 100, 50, 50, load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True))
-    # blocks = [Block(0, HEIGHT - block_size, block_size, get_block)]
-    floor = [Block(i * block_size, HEIGHT - block_size, block_size, get_block) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
-    fire = Fire(100, HEIGHT - block_size - 64, 16, 32, load_sprite_sheets)
-    fire.on()
-    objects = [*floor,
-               Block(0, HEIGHT - block_size * 2, block_size, get_block),
-               Block(block_size * 3, HEIGHT - block_size * 4, block_size, get_block),
-               Block(block_size * 4, HEIGHT - block_size * 4, block_size, get_block),
-               Block(block_size * 6, HEIGHT - block_size * 5, block_size, get_block),
-               fire]
-    
+    background, bg_image = get_background("Blue.png")    
 
     offset_x = 0
     scroll_area_width = 200
+
+    # Objetos
+    player = Player(100, 100, 50, 50, load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True))
+    # blocks = [Block(0, HEIGHT - block_size, block_size, get_block)]
+    block_size = 96
+    floor = [Block(i * block_size, HEIGHT - block_size, block_size, get_block) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
+    fire = Fire(100, HEIGHT - block_size - 64, 16, 32, load_sprite_sheets)
+    fire.on()
+    objects = [
+        *floor,
+        Block(0, HEIGHT - block_size * 2, block_size, get_block),
+        Block(block_size * 3, HEIGHT - block_size * 4, block_size, get_block),
+        Block(block_size * 4, HEIGHT - block_size * 4, block_size, get_block),
+        Block(block_size * 6, HEIGHT - block_size * 5, block_size, get_block),
+        fire
+    ]    
 
     run = True
     while run:
