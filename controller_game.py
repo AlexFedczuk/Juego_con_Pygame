49a40c20de227a0d8856db33bc54ddd012ee2fb1
Fedczuk Alex <afedczuk@indec.gob.ar>
2023-12-04 14:00:31 -0300

    good

--- controller_game.py
+++ controller_game.py
@@ -1,6 +1,6 @@
 import pygame
 
-from constants import WINDOW, FPS, FONT_PATH, CLOSE_BUTTON_IMG_PATH, ICE_PARTICLE_IMG_PATH, MUD_PARTICLE_IMG_PATH, PLAY_RECT_PATH
+from constants import *
 from funtions import *
 
 from class_player import Player
@@ -37,13 +37,12 @@ def controller_play_game():
     while runing:
         clock.tick(FPS)
         elapsed_time = pygame.time.get_ticks() - start_time
-
-        """events_list = pygame.event.get()
-        runing = check_events(events_list, pygame.mouse.get_pos(), player, controller_pause_menu, EXIT_BUTTON, tecla_f1)"""
         events_list = pygame.event.get()
         mouse_position = pygame.mouse.get_pos()
+
         coins = [obj for obj in objects if isinstance(obj, Coin)]
         runing = controller_ending_menu(player.live_status(), enemies, coins, (time - (elapsed_time // 1000)))
+
         for event in events_list:
             if event.type == pygame.QUIT:
                 print("Cerrando juego.")
@@ -58,9 +57,7 @@ def controller_play_game():
                 if len(player.proyectiles_shooted) < 3:
                     player.proyectiles_shooted.append(player.create_proyectile(player.proyectile_image_path, player.direction))
                 if EXIT_BUTTON.check_input(mouse_position):
-                    runing = False
-            
-            
+                    runing = False            
         controller_loop(player, enemies, objects, TIMER)
         handle_movement(player, objects, enemies, offset_x)        
         draw(WINDOW, background, bg_image, player, objects, offset_x, enemies, EXIT_BUTTON)
@@ -69,5 +66,4 @@ def controller_play_game():
         offset_x = scroll_screen(player, offset_x, scroll_area_width)
 
         draw_rectangle(tecla_f1, WINDOW, player, objects, enemies, offset_x)
-        #print(f"X: {player.rect.x} - Y: {player.rect.y} - Is he dead? {player.dead}"
         pygame.display.update()
